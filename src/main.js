f05e54b86c509a746196c6188432d0b75ccc2d92
Alexander Staroselsky <34102969+alexstaroselsky@users.noreply.github.com>
2020-02-23 19:25:41 -0700

    fix: updated svelte target
    Updated svelte app target to be more unique to extention.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body
+	target: document.getElementById('microsoft-edge-extension-lorem-ipsum')
 });
 
 export default app;
